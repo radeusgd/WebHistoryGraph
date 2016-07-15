ea758af8390cf8ef48cb8a0ef1f9cc395a1e47b9
Radosław Waśko <programistagd@gmail.com>
2016-07-15 15:37:39 +0200

    Redid website management, introduced titles, doubleclick to open. Experimented with display methods (hierarchy disabled for now).

--- src/browser_action/popup.js
+++ src/browser_action/popup.js
@@ -1,50 +1,113 @@
-/*// create an array with nodes
-var nodes = new vis.DataSet([
-{id: 1, label: 'Node 1'},
-{id: 2, label: 'Node 2'},
-{id: 3, label: 'Node 3'},
-{id: 4, label: 'Node 4'},
-{id: 5, label: 'Node 5'}
-]);
-
-// create an array with edges
-var edges = new vis.DataSet([
-{from: 1, to: 3},
-{from: 1, to: 2},
-{from: 2, to: 4},
-{from: 2, to: 5}
-]);*/
-function nodes_prepare_for_graphing(sites){
-  var nodes = [];
-  //TODO
-  for(var i=0;i<sites.length;++i){
-    nodes.push({id:sites[i].id,label:sites[i].label,shape:'box',level:Math.floor(Math.random()*20)});//TODO levels
-  }
-  return nodes;
+function debug(msg){
+    $("#dbgmsg").html($("#dbgmsg").html()+msg+"<br>");
 }
-
-var nodes = new vis.DataSet(nodes_prepare_for_graphing(chrome.extension.getBackgroundPage().sites));
-var edges = new vis.DataSet(chrome.extension.getBackgroundPage().links);
-// create a network
-var container = document.getElementById('mynetwork');
+function trim(str){
+    var maxLen = 45;
+    if(str.length > maxLen){
+        return str.substr(0,maxLen-3)+"...";
+    }
+    return str;
+}
+function prepareGraph(sites,links){
+    var nodes = [];
+    var edges = links;
+    var outs = [];
+    var ins = [];
+    for(var i=0;i<sites.length;++i){
+        debug(JSON.stringify(sites[i]));
+        nodes.push({id:sites[i].id,label:trim(sites[i].title),shape:'box',level:undefined});
+        outs.push([]);
+        ins.push([]);
+    }
+    for(var i=0;i<edges.length;++i){
+        var e = edges[i];
+        outs[e.from].push(e.to);
+        ins[e.to].push(e.from);
+    }
+    /*//now compute levels: a kind of BFS - start with 0 and populate
+    var BFS = function(startid){
+        nodes[startid].level = 0;
+        var queue = [startid];
+        while(queue.length>0){
+            var c = queue.shift();//pop
+            for(var i=0;i<outs[c].length;++i){
+                if(nodes[outs[c][i]].level === undefined){//TODO maybe add OR abs()<abs()???
+                    nodes[outs[c][i]].level = nodes[c].level+1;
+                    queue.push(outs[c][i]);
+                }
+            }
+            for(var i=0;i<ins[c].length;++i){
+                if(nodes[ins[c][i]].level === undefined){//TODO maybe add OR abs()<abs()???
+                    nodes[ins[c][i]].level = nodes[c].level-1;
+                    queue.push(ins[c][i]);
+                }
+            }
+        }
+    };
+    for(var i=0;i<nodes.length;++i){
+        if(nodes[i].level === undefined){
+            BFS(i);
+        }
+    }*///level computing is disabled for now due to unsatisfactory results, currently we rely on physics
+    return {nodes:nodes,edges:edges};
+}
+var graph = prepareGraph(chrome.extension.getBackgroundPage().sites,chrome.extension.getBackgroundPage().links);
+var nodes = new vis.DataSet(graph.nodes);
+var edges = new vis.DataSet(graph.edges);
+var container = document.getElementById('webgraph');
 var data = {
-   nodes: nodes,
-   edges: edges
+    nodes: nodes,
+    edges: edges
 };
 var options = {
-   layout: {
-      hierarchical: {
-         sortMethod: 'directed ',
-         direction: 'UD',
-         levelSeparation: 20,
-         nodeSpacing: 10,
-         treeSpacing: 50,
-         parentCentralization: false
-      }
-   },
-   edges: {
-      smooth: true,
-      arrows: {to : true }
-   }
+    layout: {
+        randomSeed: 0
+        /*hierarchical: {
+            direction: 'LR',
+            levelSeparation: 50,
+            nodeSpacing: 30,
+            treeSpacing: 80,
+            parentCentralization: true
+        }*/
+    },
+    edges: {
+        smooth: true,
+        arrows: {to : true }
+    },
+    physics:{
+        barnesHut: {
+            gravitationalConstant: -2000,
+            centralGravity: 0.4,
+            springLength: 25,
+            springConstant: 0.01,
+            damping: 0.2,
+            avoidOverlap: 0.7
+        },
+        hierarchicalRepulsion: {
+            centralGravity: 0.1,
+            springLength: 90,
+            springConstant: 0.01,
+            nodeDistance: 120,
+            damping: 0.09
+        },
+    }
 };
-var network = new vis.Network(container, data, options);
+
+$(function() {
+    var network = new vis.Network(container, data, options);
+    network.on( 'doubleClick', function(properties) {
+        var url = chrome.extension.getBackgroundPage().sites[properties.nodes].url;
+        //TODO check current tabs and for uids and if they match, switch to tab instead of opening a new one
+        chrome.tabs.create({ url: url });
+    });
+    $("#clear").click(function(){
+        chrome.extension.getBackgroundPage().clearMemory();
+        window.location.href="";
+    });
+    $("#wiki-only").change(function() {
+        chrome.extension.getBackgroundPage().wikiOnly = this.checked;
+        debug("set to: "+chrome.extension.getBackgroundPage().wikiOnly);
+    });
+    debug("loaded as: "+chrome.extension.getBackgroundPage().wikiOnly);
+    $("#wiki-only").attr("checked",chrome.extension.getBackgroundPage().wikiOnly);
+});
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -9,14 +9,13 @@ function removeTrail(old){
 	if(index != -1){
 		res = old.substring(0, index);
 	}
-	//index = res.indexOf("//");
-	//res = res.substring(index+2);
+	index = res.indexOf("//");
+	res = res.substring(index+2);
 	return res;
 }
 
 $(document).ready(function(){
-	var from = removeTrail(document.referrer);
-	var to = removeTrail(document.URL);
-	console.log(from+" -> "+to);
+	var from = {url:document.referrer, uid:removeTrail(document.referrer), title: removeTrail(document.referrer)};//we use placeholder title as there's no other way to know it
+	var to = {url:document.URL, uid: removeTrail(document.URL), title: document.title};
 	chrome.extension.sendMessage({from:from,to:to});
 });
